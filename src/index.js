ac679af6ca08612254bc3f86b076375f372e2ad5
suganthssp <45447346+suganthssp@users.noreply.github.com>
2020-04-26 16:29:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My Favourites</h1>
+    <ul>
+      <li>cheese</li>
+      <li>biriyani</li>
+      <li>Dosa</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
